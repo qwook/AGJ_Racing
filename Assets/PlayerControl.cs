9fdc3ff8d25a00d57f09c0dd8d886de4d45bcf8c
jTack <Takumi@TKMMBA.local>
2014-09-13 13:58:32 -0700

    made wheeling work

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -2,16 +2,21 @@
 using System.Collections;
 
 public class PlayerControl : MonoBehaviour {
-	
-	public float steering = 0;
+
+	GameObject userInterface;
+	SteeringUI steeringUI;
+	float steering;
 
 	// Use this for initialization
 	void Start () {
-
+		userInterface = GameObject.Find("UnityWatermark-small");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		steeringUI = userInterface.GetComponent<SteeringUI>();
+		steering = steeringUI.angle;
 
 		//  Key Control
 		if (Input.GetKey ("w")) {
@@ -28,17 +33,17 @@ public class PlayerControl : MonoBehaviour {
 		}
 
 		// Simulating steering wheel's value
-		if (Input.GetKey ("7")) {
-				steering = 360.0f;
-		} else if (Input.GetKey ("8")) {
-				steering = 180.0f;
-		} else if (Input.GetKey ("9")) {
-				steering = -180.0f;
-		} else if (Input.GetKey ("0")) {
-				steering = -360.0f;
-		} else {
-			steering = 0;		
-		}
+//		if (Input.GetKey ("7")) {
+//				steering = 360.0f;
+//		} else if (Input.GetKey ("8")) {
+//				steering = 180.0f;
+//		} else if (Input.GetKey ("9")) {
+//				steering = -180.0f;
+//		} else if (Input.GetKey ("0")) {
+//				steering = -360.0f;
+//		} else {
+//			steering = 0;		
+//		}
 
 		// Wheel Control
 		if (steering > 0 && (Input.GetKey ("w") || Input.GetKey ("s"))) {
