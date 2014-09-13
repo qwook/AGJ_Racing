c75e8b3e49e7399821e5276e54a23096b83014d5
Henry <henryquoctran@gmail.com>
2014-09-13 14:53:12 -0700

    Added bezier curves

--- Assets/PathMaker.cs
+++ Assets/PathMaker.cs
@@ -24,19 +24,32 @@ public class PathMaker : MonoBehaviour {
 	}
 
 	// Modified Bezier Curve
-	void Curve() {
+	List<Vector3> Curve(Vector3 start, Vector3 end, Vector3 third) {
+		List<Vector3> points = new List<Vector3>();
 
+		for (int i = 1; i < sectors; i++) {
+			float interval = (float)i / (float)sectors;
+
+			Vector3 a = Vector3.Lerp( start, third, interval );
+			Vector3 b = Vector3.Lerp( third, end, interval );
+
+			Vector3 c = Vector3.Lerp(a, b, interval);
+
+			points.Add(c);
+		}
+		points.Add(end);
+
+		return points;
 	}
 
 	static int sectors = 10;
-	void DrawCurve(Vector3 start, Vector3 end) {
+	void DrawCurve(Vector3 start, Vector3 end, Vector3 thirdChild) {
+		List<Vector3> points = Curve(start, end, thirdChild);
 
-		Vector3 last = start;
-		for (int i = 1; i < sectors; i++) {
-			float interval = i / sectors;
-
-			Vector3 delta = end-last;
-			// Gizmos.DrawLine(last, );
+		Vector3 lastPoint = start;
+		foreach (Vector3 point in points) {
+			Gizmos.DrawLine(lastPoint, point);
+			lastPoint = point;
 		}
 	}
 
@@ -49,23 +62,22 @@ public class PathMaker : MonoBehaviour {
 
 		for (int i = 0; i < children.Count; i++) {
 			Transform child = children[i];
+
 			var nextChild = children[(i+1) % children.Count];
+			var thirdChild = child.Find("CurveControl");
 
 	        Gizmos.color = Color.yellow;
-	        Gizmos.DrawLine(child.position, nextChild.position);
+	        // Gizmos.DrawLine(child.position, nextChild.position);
 
-	        var delta = nextChild.position - child.position;
-	        var midPoint = child.position + delta/2;
+	        var delta = thirdChild.position - child.position;
 
 	        Vector3 up = new Vector3(0, 1, 0);
 	        Vector3 perp = Vector3.Cross(delta, up).normalized;
 
-	        Gizmos.DrawLine(midPoint, midPoint - delta.normalized*1 + perp);
-	        Gizmos.DrawLine(midPoint, midPoint - delta.normalized*1 - perp);
+	        Gizmos.DrawLine(child.position, child.position - delta.normalized*1 + perp);
+	        Gizmos.DrawLine(child.position, child.position - delta.normalized*1 - perp);
 
-			// DrawCurve(child.position, nextChild.position);
-
-	        // Handles.DrawBezier(child.position, nextChild.position, nextChild.position, nextChild.position, Color.red, null, 3);
+			DrawCurve(child.position, nextChild.position, thirdChild.position);
 		}
 	}
 }
