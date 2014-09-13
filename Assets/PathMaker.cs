5ec1183749c12de3629411bfe621daedcefd2818
Henry <henryquoctran@gmail.com>
2014-09-13 15:45:18 -0700

    two controller for bezier curve

--- Assets/PathMaker.cs
+++ Assets/PathMaker.cs
@@ -24,18 +24,22 @@ public class PathMaker : MonoBehaviour {
 	}
 
 	// Modified Bezier Curve
-	List<Vector3> Curve(Vector3 start, Vector3 end, Vector3 third) {
+	List<Vector3> Curve(Vector3 start, Vector3 end, Vector3 controlA, Vector3 controlB) {
 		List<Vector3> points = new List<Vector3>();
 
 		for (int i = 1; i < sectors; i++) {
 			float interval = (float)i / (float)sectors;
 
-			Vector3 a = Vector3.Lerp( start, third, interval );
-			Vector3 b = Vector3.Lerp( third, end, interval );
+			Vector3 a = Vector3.Lerp( start, controlA, interval );
+			Vector3 b = Vector3.Lerp( controlA, controlB, interval );
+			Vector3 c = Vector3.Lerp( controlB, end, interval );
+			
+			Vector3 d = Vector3.Lerp( a, b, interval );
+			Vector3 e = Vector3.Lerp( b, c, interval );
 
-			Vector3 c = Vector3.Lerp(a, b, interval);
+			Vector3 f = Vector3.Lerp( d, e, interval );
 
-			points.Add(c);
+			points.Add(f);
 		}
 		points.Add(end);
 
@@ -43,8 +47,8 @@ public class PathMaker : MonoBehaviour {
 	}
 
 	static int sectors = 10;
-	void DrawCurve(Vector3 start, Vector3 end, Vector3 thirdChild) {
-		List<Vector3> points = Curve(start, end, thirdChild);
+	void DrawCurve(Vector3 start, Vector3 end, Vector3 controlA, Vector3 controlB) {
+		List<Vector3> points = Curve(start, end, controlA, controlB);
 
 		Vector3 lastPoint = start;
 		foreach (Vector3 point in points) {
@@ -64,12 +68,17 @@ public class PathMaker : MonoBehaviour {
 			Transform child = children[i];
 
 			var nextChild = children[(i+1) % children.Count];
-			var thirdChild = child.Find("CurveControl");
+			var controlA = child.Find("CurveControlA");
+			var controlB = child.Find("CurveControlB");
+
+	        Gizmos.color = Color.red;
+
+	        Gizmos.DrawLine(child.position, controlA.position);
+	        Gizmos.DrawLine(nextChild.position, controlB.position);
 
 	        Gizmos.color = Color.yellow;
-	        // Gizmos.DrawLine(child.position, nextChild.position);
 
-	        var delta = thirdChild.position - child.position;
+	        var delta = controlA.position - child.position;
 
 	        Vector3 up = new Vector3(0, 1, 0);
 	        Vector3 perp = Vector3.Cross(delta, up).normalized;
@@ -77,7 +86,7 @@ public class PathMaker : MonoBehaviour {
 	        Gizmos.DrawLine(child.position, child.position - delta.normalized*1 + perp);
 	        Gizmos.DrawLine(child.position, child.position - delta.normalized*1 - perp);
 
-			DrawCurve(child.position, nextChild.position, thirdChild.position);
+			DrawCurve(child.position, nextChild.position, controlA.position, controlB.position);
 		}
 	}
 }
