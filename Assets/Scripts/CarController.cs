447b6face58c58be214b7b9c202eac064b7350c8
Henry <henryquoctran@gmail.com>
2014-09-13 17:08:28 -0700

    Created Small test CPU car

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float angle = 0.0f;
+
+		if (Input.GetKey(KeyCode.A)) {
+			angle = -5.0f;
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			angle = 5.0f;
+		}
+
+		if (Input.GetKey(KeyCode.W)) {
+			rigidbody.AddRelativeForce(Vector3.forward*20.0f, ForceMode.Acceleration);
+			rigidbody.AddRelativeTorque(Vector3.up*angle, ForceMode.Acceleration);
+		}
+
+		if (Input.GetKey(KeyCode.S)) {
+			rigidbody.AddRelativeForce(Vector3.forward*-20, ForceMode.Acceleration);
+			rigidbody.AddRelativeTorque(Vector3.up*-angle, ForceMode.Acceleration);
+		}
+
+	}
+}
